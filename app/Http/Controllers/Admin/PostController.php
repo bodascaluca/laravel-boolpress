0b58ec0f7bac9381b6296ceb5919383386614e85
bodascaluca <bodasca.luca@gmail.com>
2022-07-06 17:45:30 +0200

    Implementato index sulla tabellla Posts

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        // dd('index');
+        $posts = Post::all();
+        // dd($posts);
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function() {
         Route::get('/', 'HomeController@index')->name('home');
-        Route::resources('posts', 'PostController');
+        Route::resource('posts', 'PostController');
     });
 
 Route::get('{any?}', function(){
