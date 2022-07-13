1d40a224cb3faca340dc5bbf466ef0fe826af088
bodascaluca <bodasca.luca@gmail.com>
2022-07-13 19:03:29 +0200

    Aggint cue cli singler page, home/about/Blog con i rlarivi conmponets

---
+++ resources/js/components/BannerT.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div class="container-fluid">
+    <h2>Questo è il banner</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "BannerT"
+}
+</script>
+
+<style>
+</style>
---
+++ resources/js/pages/Blog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <BannerT />
+    <h1>Questo è il mio blog</h1>
+    <Posts />
+  </div>
+</template>
+
+<script>
+import BannerT from "../components/BannerT.vue";
+import Posts from "../components/Posts.vue";
+
+export default {
+    name: "Blog",
+    components: {
+        Posts,
+        BannerT
+    }
+}
+</script>
+
+<style>
+</style>
